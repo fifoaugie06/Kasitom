bbedb8aa84fd41af475324a5b4aea6401b9aeccf
AUGIE-PC <fifoaugie06@gmail.com>
2019-12-07 21:53:06 +0700

    KasitomV1.2

--- app/src/main/java/com/example/kasitom/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kasitom/ui/home/HomeFragment.java
@@ -5,31 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.kasitom.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeFragment extends Fragment {
-
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        CardView cardView = view.findViewById(R.id.btn_antonim);
+        cardView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Berhasil", Toast.LENGTH_LONG).show();
             }
         });
-        return root;
+
+
+        return view;
     }
 }
